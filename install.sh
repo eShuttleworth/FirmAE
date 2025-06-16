5994483e371797c3fa7c3ad8c539aea5e0d26e57
Edwin Shuttleworth <edwin@finitestate.io>
2025-06-16 16:18:42 -0500

    tweak deps

--- install.sh
+++ install.sh
@@ -24,13 +24,22 @@ python3 -m pip install psycopg2 psycopg2-binary
 
 sudo apt install -y busybox-static bash-static fakeroot dmsetup kpartx netcat-openbsd nmap python3-psycopg2 snmp uml-utilities util-linux vlan
 
-# for binwalk
-wget https://github.com/ReFirmLabs/binwalk/archive/refs/tags/v2.3.4.tar.gz && \
-  tar -xf v2.3.4.tar.gz && \
-  cd binwalk-2.3.4 && \
-  sed -i 's/^install_ubireader//g' deps.sh && \
-  echo y | ./deps.sh && \
-  sudo python3 setup.py install
+# # for binwalk
+# wget https://github.com/ReFirmLabs/binwalk/archive/refs/tags/v2.3.4.tar.gz && \
+#   tar -xf v2.3.4.tar.gz && \
+#   cd binwalk-2.3.4 && \
+#   sed -i 's/^install_ubireader//g' deps.sh && \
+#   echo y | ./deps.sh && \
+#   cd sasquatch && ./build.sh && \
+#   sudo python3 setup.py install
+#   cd ..
+
+git clone https://github.com/eShuttleworth/binwalk.git
+cd binwalk
+echo y | ./deps.sh
+sudo python3 setup.py install
+cd -
+
 sudo apt install -y mtd-utils gzip bzip2 tar arj lhasa p7zip p7zip-full cabextract fusecram cramfsswap squashfs-tools sleuthkit default-jdk cpio lzop lzma srecord zlib1g-dev liblzma-dev liblzo2-dev unzip
 
 cd - # back to root of project
@@ -40,20 +49,20 @@ sudo cp core/unstuff /usr/local/bin/
 python3 -m pip install python-lzo cstruct ubi_reader
 sudo apt install -y python3-magic openjdk-8-jdk unrar
 
-# for analyzer, initializer
-sudo apt install -y python3-bs4
-python3 -m pip install selenium
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb; sudo apt -fy install
-rm google-chrome-stable_current_amd64.deb
-python3 -m pip install -r ./analyses/routersploit/requirements.txt
-cd ./analyses/routersploit && patch -p1 < ../routersploit_patch && cd -
+# # for analyzer, initializer
+# sudo apt install -y python3-bs4
+# python3 -m pip install selenium
+# wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+# sudo dpkg -i google-chrome-stable_current_amd64.deb; sudo apt -fy install
+# rm google-chrome-stable_current_amd64.deb
+# python3 -m pip install -r ./analyses/routersploit/requirements.txt
+# cd ./analyses/routersploit && patch -p1 < ../routersploit_patch && cd -
 
 # for qemu
 sudo apt install -y qemu-system-arm qemu-system-mips qemu-system-x86 qemu-utils
 
-if ! test -e "./analyses/chromedriver"; then
-    wget https://chromedriver.storage.googleapis.com/2.38/chromedriver_linux64.zip
-    unzip chromedriver_linux64.zip -d ./analyses/
-    rm -rf chromedriver_linux64.zip
-fi
+# if ! test -e "./analyses/chromedriver"; then
+#     wget https://chromedriver.storage.googleapis.com/2.38/chromedriver_linux64.zip
+#     unzip chromedriver_linux64.zip -d ./analyses/
+#     rm -rf chromedriver_linux64.zip
+# fi
